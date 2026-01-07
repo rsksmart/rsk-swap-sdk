fba250a921ee31367fec7fa593b2a66aebb2ff02
lserra-iov <117093501+lserra-iov@users.noreply.github.com>
2026-01-07 17:16:46 -0300

    Update Boltz validation (#67)
    * chore: update ether swap bytecode hash validation
    
    * chore: update package

--- src/constants/validation.ts
+++ src/constants/validation.ts
@@ -6,7 +6,7 @@ export const VALIDATION_CONSTANTS = deepFreeze({
       etherSwapBytecodeHash: '0x000c3dfadd394049d31e38889399d6ee7ce1c506383ac5c3275b44ef0b94931f'
     },
     mainnet: {
-      etherSwapBytecodeHash: '0xfb8d82d533d06bb31f373d9f1d6c28ce371605c62e1889b8c068da9a433e0a57'
+      etherSwapBytecodeHash: '0xfba7d5f8c66a248ee4f867cb46460b6d248bddafdb0546ae780f3d59480a155f'
     }
   }
 } as const)
